09ef4aee35b4891759bdfe9247dc1498803c1390
FraintSaad <islamqwertyu@mail.ru>
2025-07-23 11:01:13 +0300

    1

--- CinemaApp/Models/FilmResponse.cs
+++ CinemaApp/Models/FilmResponse.cs
@@ -6,13 +6,8 @@ namespace CinemaApp.Models
 {
     public class FilmResponse
     {
-        [JsonProperty("total")]
         public int Total { get; set; }
-
-        [JsonProperty("totalPages")]
         public int TotalPages { get; set; }
-
-        [JsonProperty("items")]
         public List<FilmModel>? Items { get; set; }
     }
 }
